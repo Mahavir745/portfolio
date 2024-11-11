de9237cf8a863319f504035a19dee484842087ba
Mahavir745 <mahavir777kumar@gmail.com>
2024-11-11 09:27:49 +0530

    new features has been added

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 const aboutUs = document.querySelector("#aboutUs")
 const menu = document.querySelector(".header_container #child1 #child1_items #element1")
 const child1 = document.querySelector(".main_container #child1")
+const footerContainer = document.querySelector(".footerContainer")
 
 menu.addEventListener("click",()=>{
   child1.classList.toggle("menu_bar")
 })
+
+setInterval(()=>{
+  footerContainer.classList.toggle("footerAnimation")
+},3000)
